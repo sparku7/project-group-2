76e816e9ce9ef2d5b863cd1b804399b7b1ab8ae7
SanaAhmad1988 <sanaahmad1@hotmail.com>
2024-05-30 11:48:34 +0100

    added navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function NavBar() {
+  return (
+    <>
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="/">Wrong Move</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/buyers">Buyers</Nav.Link>
+            <Nav.Link href="/sellers">Sellers</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <br />
+     </>
+  );
+}
+
+export default NavBar;
---
+++ src/pages/homepage.jsx
@@ -0,0 +1,14 @@
+function HomePage(){
+
+    return(
+<div>
+
+</div>
+
+
+
+    )
+}
+
+
+export default HomePage;
