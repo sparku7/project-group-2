b2b328cfab0464bf8ade00c054af814c8f11a08b
PaulHorgan <horho@hotmail.co.uk>
2024-05-30 15:06:36 +0100

    commit with homepage and property.json

---
+++ src/components/PropertyCard.jsx
@@ -0,0 +1,29 @@
+import '../App.css'
+
+export default function PlantCard({street, town, bedrooms, bathrooms, price, garden, imageUrl}) {
+
+    return (
+
+        <div className="card">
+            <h2> {town} </h2>
+            <img className="card-img" src={imageUrl}  />
+            <h4> Bedrooms : {bedrooms} </h4>
+            <p> Bathrooms : {bathrooms}</p>
+            <p> Garden? Y/N : {garden} </p>
+            <p> Address : {street} </p>
+            <h2> Price : £{price} </h2>
+        </div>
+    ) 
+
+
+}
+
+// id":"1",
+//             "street":"123 avenue",
+//             "town":"Dunfermline",
+//             "bedrooms":3,
+//             "bathrooms":2,
+//             "price":123123,
+//             "garden":"yes",
+//             "imageUrl":"test.com",
+//             "status":"for sale"
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,59 @@
+import PlantCard from "../components/PropertyCard";
+import houseData from '../data/properties.json'
+import '../App.css'
 
 
 
 
-function HomePage(){
+function HomePage() {
+    return (
+        <div className="body">
+            <div className="head-image" height="600px">
+                <img margin="collapse" height="640px" width="75%" src="https://c1.wallpaperflare.com/preview/989/964/872/castle-church-monastery-architecture.jpg" />
+                <div margin="collapse" background-color="blue" className="searchplaceholder" height="640px" width="25%">
+                    <p> extra <textarea name="" id=""></textarea></p>
+                </div>
+            </div>
+            <div className="items-grid">
 
-    return(
-        
-        <div>
-
+                {houseData.map((item) => (
+                    <PlantCard
+                        street={item.street}
+                        town={item.town}
+                        bedrooms={item.bathrooms}
+                        bathrooms={item.bathrooms}
+                        price={item.price}
+                        garden={item.garden}
+                        imageUrl={item.imageUrl}
+                    />
+                ))}
+            </div>
         </div>
-
     )
 }
 
 
 export default HomePage;
+
+
+// import itemsData from "../itemsData.json";
+// import PlantCard from '../components/PlantCard';
+
+// const ShopPage = () => {
+//     return(
+//         <div className="body">
+//             <h1 className="shopHeader">Our Plants</h1>
+//             <div className="shopItems">
+//                 {itemsData.map((item) => (
+//                         <PlantCard
+//                         name = {item.name}
+//                         price = {item.price}
+//                         imageUrl = {item.imageUrl}
+//                         />
+//                     ))}
+//             </div>
+//         </div>
+//     )
+// };
+
+// export default ShopPage;
