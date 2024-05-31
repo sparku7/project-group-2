b196edc9635bdf874dfd4d8bbdf9c14e60fbbc0d
SanaAhmad1988 <sanaahmad1@hotmail.com>
2024-05-31 09:39:49 +0100

    add seller

--- src/components/AddSeller.jsx
+++ src/components/AddSeller.jsx
@@ -1,1 +1,96 @@
+import { Alert } from "bootstrap";
+import { useState } from "react"; 
 
+import { useNavigate } from "react-router-dom"; 
+
+
+
+ 
+
+const AddSeller = () => { 
+
+  const [firstName, setFirstname] = useState(''); 
+
+  const [surname, setSurname] = useState(''); 
+
+  const navigate = useNavigate(); 
+
+ 
+
+  const handleSubmit = (e) => { 
+
+    e.preventDefault(); 
+
+    const addSeller = { firstName, surname }; 
+
+ 
+
+    fetch('http://localhost:8000/sellers', { 
+
+      method: 'POST', 
+
+      headers: { "Content-Type": "application/json" }, 
+
+      body: JSON.stringify(addSeller) 
+
+    })
+    
+    alert("Seller Added")
+
+    
+
+  } 
+
+ 
+
+  return ( 
+
+    <div className="addseller"> 
+
+      <h2>Register A New Seller</h2> 
+
+      <form onSubmit={handleSubmit}> 
+     <div> 
+        <label>Firstname</label>
+        
+
+        <input  
+
+          type="text"  
+
+          required  
+
+          value={firstName} 
+
+          onChange={(e) => setFirstname(e.target.value)} 
+
+        />  
+  </div>
+  <br />
+  <div> <label>Surname:</label> 
+
+        <input 
+
+        type="text" 
+
+        value={surname} 
+
+        onChange={(e) => setSurname(e.target.value)} 
+
+/> 
+</div>
+  
+        <br />
+        <button>Submit</button> 
+
+      </form> 
+    
+    </div> 
+
+  ); 
+
+} 
+
+  
+
+export default AddSeller; 
--- src/pages/Sellers.jsx
+++ src/pages/Sellers.jsx
@@ -1,20 +1,86 @@
+import { useNavigate, useParams } from "react-router-dom"; 
+
+import { useState, useEffect } from "react"; 
+
+import AddSeller from "../components/AddSeller";
+import Table from 'react-bootstrap/Table';
 
 
+ 
 
+const Sellers = ()=> { 
 
+  const { id } = useParams(); 
 
+  const [sellers, setSellers] = useState([]) 
 
+  const navigate = useNavigate(); 
 
-function Sellers(){
+ 
 
-    return(
+  const handleClick = () => { 
+
+    fetch('http://localhost:8000/sellers/' + sellers.id, { 
+
+      method: 'DELETE' 
+
+    }).then(() => { 
+
+      navigate('/sellers'); 
+
+    })  
+
+  } 
+
+ 
+
+  useEffect(() =>{ 
+
+    fetch('http://localhost:8000/sellers') 
+
+      .then((res) => res.json()) 
+
+      .then((data) =>setSellers(data) ) 
+
+  }, [sellers]) 
+
+ 
+  return (
+    <div>
+     <AddSeller/>
+
+    <br/> <br />
+
+    { sellers.map((sell)=>( 
+    <Table striped="columns">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Surame</th>
+          <th>Delete Seller</th>
         
-        <div>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{sell.id}</td>
+          <td>{sell.firstName}</td>
+          <td>{sell.surname}</td>
+          <td><button onClick={handleClick}>delete</button></td>
 
-        </div>
+       
+        </tr>
+    
+      
+      </tbody>
+    </Table>
 
-    )
+))} 
+    </div>
+  );
 }
 
+  
 
-export default Sellers;
+export default Sellers; 
