1dad2fa24ddb4ff765e962ce3d1b2217bacbedc0
sparku7 <sparku7@sky.com>
2024-06-03 11:21:34 +0100

    Added comments

--- src/pages/Buyers.jsx
+++ src/pages/Buyers.jsx
@@ -1,23 +1,20 @@
 import AddBuyer from "../components/AddBuyer";
 import JsonDataDisplay from "../components/BuyerTable";
-import '../css/RegisterUser.css'
-
-
+import '../css/RegisterUser.css';
 
 function AddBuyers() {
 
     return (
 
         <div className="container1">
+
             <h1>Register a New Buyer</h1>
 
             <AddBuyer />
             <JsonDataDisplay />
 
         </div>
-
-
-    )
+    );
 }
 
 
