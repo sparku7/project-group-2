6c704693a0cc359b4f7e7e59cec7496c05d4c018
sparku7 <sparku7@sky.com>
2024-06-05 10:48:42 +0100

    added password to view buyers table

--- src/pages/Buyers.jsx
+++ src/pages/Buyers.jsx
@@ -1,23 +1,48 @@
+import React, { useState } from 'react';
 import AddBuyer from "../components/AddBuyer";
 import JsonDataDisplay from "../components/BuyerTable";
 import '../css/RegisterUser.css';
 
 function AddBuyers() {
+    const [password, setPassword] = useState('');
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handlePasswordSubmit = (e) => {
+        e.preventDefault();
+        if (password === 'Password') {
+            setIsAuthenticated(true);
+        } else {
+            alert('Incorrect password');
+        }
+    };
 
     return (
-
         <div className="body">
             <div className="container1">
+                <h1>Register a New Buyer</h1>
+                <AddBuyer />
+                {!isAuthenticated && (
+                    <form className='password-form' onSubmit={handlePasswordSubmit}>
+                        <label>  Enter Admin Password to see list of Buyers  </label>
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={handlePasswordChange}
+                        />
+                        <br></br>
+                        <br></br>
 
-            <h1>Register a New Buyer</h1>
-
-            <AddBuyer />
-            <JsonDataDisplay />
-
+                        <button type="submit">Submit</button>
+                    </form>
+                )}
+                {isAuthenticated && <JsonDataDisplay />}
             </div>
         </div>
     );
 }
-
 
 export default AddBuyers;
